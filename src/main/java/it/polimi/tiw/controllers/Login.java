35dc8acd129fdc9861b5e1ec42a1a2357ef887c8
lorenzofranze <10679981@polimi.it>
2022-05-09 15:48:15 +0200

    update

--- src/main/java/it/polimi/tiw/controllers/Login.java
+++ src/main/java/it/polimi/tiw/controllers/Login.java
@@ -1,4 +1,82 @@
 package it.polimi.tiw.controllers;
 
-public class Login {
+import it.polimi.tiw.DAO.*;
+import it.polimi.tiw.beans.*;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import it.polimi.tiw.utils.*;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private Connection connection = null;
+    private TemplateEngine templateEngine;
+
+    public void init() throws ServletException {
+        connection = ConnectionHandler.getConnection(getServletContext());
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parameters incomplete");
+            return;
+        }
+
+
+        UserDAO userDao = new UserDAO(connection);
+        User user = null;
+
+        try {
+            user = userDao.checkUser(username, password);
+        } catch (SQLException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not Possible to check credentials");
+            return;
+        }
+
+        String path;
+        if (user == null) {
+            ServletContext servletContext = getServletContext();
+            final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+            ctx.setVariable("errorMsg", "Incorrect username or password");
+            path = "/loginPage";
+            templateEngine.process(path, ctx, response.getWriter());
+        } else {
+            HttpSession session = request.getSession();
+            session.setAttribute("currentUser", user);
+            path = getServletContext().getContextPath() + "/homePage";
+            response.sendRedirect(path);
+        }
+
+    }
+
+    public void destroy() {
+        try {
+            ConnectionHandler.closeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/it/polimi/tiw/controllers/Subscribe.java
+++ src/main/java/it/polimi/tiw/controllers/Subscribe.java
@@ -1,4 +1,54 @@
 package it.polimi.tiw.controllers;
 
-public class Subscribe {
+import it.polimi.tiw.utils.ConnectionHandler;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/Subscribe")
+public class Subscribe extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private Connection connection = null;
+    private TemplateEngine templateEngine;
+
+    public void init() throws ServletException {
+        connection = ConnectionHandler.getConnection(getServletContext());
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String name = request.getParameter("name");
+        String password1 = request.getParameter("password1");
+        String password2 = request.getParameter("password2");
+
+        if(username == null || username.length()<=4 || name == null || name.isEmpty()) {
+        }
+    }
+
+    public void destroy() {
+        try {
+            ConnectionHandler.closeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
