d9c9af867f7759f3e603cc0355060d576b2a4f26
Alessandro Cogollo <alessandro.cogollo@mail.polimi.it>
2022-05-20 10:45:18 +0200

    Updated scratch.sql

--- deliveries/scratch.sql
+++ deliveries/scratch.sql
@@ -1,14 +1,14 @@
 --ho dato il nome db_docmanager allo schema solo per avere lo stesso nome nel web.xml
 
 CREATE TABLE  user (
-    username VARCHAR(16) NOT NULL PRIMARY KEY,
-    email VARCHAR(20) NOT NULL UNIQUE,
-    name VARCHAR(16) NOT NULL,
+    username VARCHAR(40) NOT NULL PRIMARY KEY,
+    email VARCHAR(40) NOT NULL UNIQUE,
+    name VARCHAR(40) NOT NULL,
     password VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE folder (
-    username VARCHAR(16) NOT NULL,
+    username VARCHAR(40) NOT NULL,
     foldername VARCHAR(40) NOT NULL,
     date DATE NOT NULL,
     PRIMARY KEY (username, foldername),
@@ -18,7 +18,7 @@ CREATE TABLE folder (
 );
 
 CREATE TABLE subfolder (
-    username VARCHAR(16) NOT NULL,
+    username VARCHAR(40) NOT NULL,
     foldername VARCHAR(40) NOT NULL,
     subfoldername VARCHAR(40) NOT NULL,
     date DATE NOT NULL,
@@ -29,11 +29,11 @@ CREATE TABLE subfolder (
 );
 
 CREATE TABLE document (
-    username VARCHAR(16) NOT NULL,
+    username VARCHAR(40) NOT NULL,
     foldername VARCHAR(40) NOT NULL,
     subfoldername VARCHAR(40) NOT NULL,
     documentname VARCHAR(40) NOT NULL,
-    type VARCHAR(16) NOT NULL,
+    type VARCHAR(40) NOT NULL,
     summary VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     body blob not null,
